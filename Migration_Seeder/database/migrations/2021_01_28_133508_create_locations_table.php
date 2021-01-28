dda5462bc441b572356ca20988d0c0bff952ba1a
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-28 14:56:50 +0100

    3 migrations with columns created

---
+++ Migration_Seeder/database/migrations/2021_01_28_133508_create_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('street', 80);
+            $table->integer('numberOfEmplooyees')->unsigned();
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
